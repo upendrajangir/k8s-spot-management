dcbe0449ea47038097214e40386d6e38e5d7a89b
Upendra Jangir <upendrajangir9@gmail.com>
2023-06-12 11:31:37 +0530

    fix: backend app logs

--- backend/sigterm_app.py
+++ backend/sigterm_app.py
@@ -1,17 +1,23 @@
 import signal
 import sys
 import time
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 
 def handle_sigterm(*args):
-    print("Received SIGTERM signal. Beginning graceful shutdown.")
+    logging.info("Received SIGTERM signal. Beginning graceful shutdown.")
     for i in range(45, 0, -1):
-        print(f"Graceful shutdown in progress. Time remaining: {i} seconds.")
+        logging.info(f"Graceful shutdown in progress. Time remaining: {i} seconds.")
         time.sleep(1)
-    print("Finished graceful shutdown.")
+    logging.info("Finished graceful shutdown.")
     sys.exit(0)
+
 
 signal.signal(signal.SIGTERM, handle_sigterm)
 
 while True:
-    print("Running...")
+    logging.info("Running...")
     time.sleep(1)
